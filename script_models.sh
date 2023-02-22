b21ba6f06c1b0b4619bef10149a030501a7e87fe
Ishaan Gupta <ixg97@case.edu>
2023-02-21 20:38:28 -0500

    working on model script

---
+++ script_models.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+<<COMMENTS
+Moves the tree-based models (Random Forest, GBM, and XGBoost) to the tree_models/ folder,
+Deletes all other models (kNN and Logistic).
+(HINT: Use a FOR statement to loop through files in the folder model_out/.
+Use a CASE statement to match on the contents of the file 'case ($cat $file)'.
+If the text contains a tree-based model name (*"Random Forest"*|*GBM*|*XGBoost*)
+then move to tree_models/ otherwise(*KNN*|*Logistic*)*KNN*|*Logistic*) delete the file.
+Create a default match that prints out 'Unknown model in FILE where FILE is the filename'.
+Then run your script.)
+COMMENTS
+
+for file in datasets/model_out/*
+do
+    echo $file
+done
